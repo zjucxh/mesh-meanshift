6ef229d2e40d88de74e7e41837a2592d9107ea7c
zjucxh <chen.xianghui@hotmail.com>
2020-11-26 21:35:36 +0800

    WIP sparse adjacency matrix generated

--- demo.py
+++ demo.py
@@ -4,22 +4,42 @@ import dualmesh as dm
 from mesh import write_mesh
 import matplotlib.pyplot as plt
 from gdist import compute_gdist as geo_dist
-
-mesh = meshio.read('./data/polyhedron.obj')
+import gdist
+mesh = meshio.read('./data/cube.obj')
 #points = mesh.points
 #faces = mesh.cells[0][1]
 #dual_mesh = dm.get_dual(mesh)
 points = np.array(mesh.points,dtype=np.float64)
 faces = np.array(mesh.cells[0][1],dtype=np.int32)
-
+print(faces)
 #print(faces)
 #meshio.write('data/dual-bunny.obj', dual_mesh)
 
 # geo-distance of mesh
-src = np.array([0],dtype=np.int32)
-dst = np.array([0,40],dtype=np.int32)
-distance = geo_dist(points, faces, source_indices=src, target_indices=dst)
-print('distance = {0}'.format(distance))
+#dst = np.array(range(len(points)),dtype=np.int32)
+#src = np.array([0],dtype=np.int32)
+#dist_array = geo_dist(points, faces, source_indices=src, target_indices=dst)
+#print('pair wise distance = {0}'.format(dist_array))
+#src = np.array([1],dtype=np.int32)
+#dist_array = geo_dist(points, faces, source_indices=src, target_indices=dst)
+#print('pair wise distance = {0}'.format(dist_array))
+#src = np.array([2],dtype=np.int32)
+#dist_array = geo_dist(points, faces, source_indices=src, target_indices=dst)
+#print('pair wise distance = {0}'.format(dist_array))
+#src = np.array([3],dtype=np.int32)
+#dist_array = geo_dist(points, faces, source_indices=src, target_indices=dst)
+#print('pair wise distance = {0}'.format(dist_array))
+
+src = np.array([0,1,2,3],dtype=np.int32)
+dst = np.array(range(len(points)),dtype=np.int32)
+dist_array = gdist.distance_matrix_of_selected_points(points, faces, src)
+print('pair wise distance = {0}'.format(dist_array))
+#src = np.array(range(len(points)), dtype=np.int32)
+#src = np.array([2,5,10], dtype=np.int32)
+#print('distance = {0}'.format(distance))
+# pair wise distance
+geo_dist_matrix = gdist.local_gdist_matrix(vertices=points, triangles=faces,max_distance=np.float('Inf')) 
+print('pair-wise matrix = {0}\n'.format(geo_dist_matrix))
 #write to file 
 
 #write_mesh('data/output.obj', dual_mesh)
--- mesh.py
+++ mesh.py
@@ -3,6 +3,11 @@ import meshio
 from gdist import compute_gdist as geo_dist
 from matplotlib import pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import open3d as o3d
+
+
+
+#o3d.geometry.TriangleMesh.compute_vertex_normals
 # write mesh to obj
 def write_mesh(filename, mesh):
     vertices = mesh.points
@@ -19,6 +24,28 @@ def write_mesh(filename, mesh):
             f.write(' {0}'.format(e[i]+1))
         f.write('\n')
 
+# normalize mesh
+def normalize(mesh):
+    vertices = mesh.points
+    x = vertices[:,0]
+    y = vertices[:,1]
+    z = vertices[:,2]
+    max_x = np.max(x)
+    max_y = np.max(y)
+    max_z = np.max(z)
+    min_x = np.min(x)
+    min_y = np.min(y)
+    min_z = np.min(z)
+    x = 2.0*(x-min_x)/(max_x-min_x) - 1.0
+    y = 2.0*(y-min_y)/(max_y-min_y) - 1.0
+    z = 2.0*(z-min_z)/(max_z-min_z) - 1.0
+    vertices[:,0]=x
+    vertices[:,1]=y
+    vertices[:,2]=z
+    mesh.points=vertices
+    return mesh
+
+
 # visualize FPS algorithms
 def fps_vis(vertices, samples):
     fig = plt.figure()
@@ -26,14 +53,18 @@ def fps_vis(vertices, samples):
     x = vertices[:,0]
     y = vertices[:,1]
     z = vertices[:,2]
-    cx = [x[i] for i in range(len(x)) if i not in samples]
-    cy = [y[i] for i in range(len(y)) if i not in samples]
-    cz = [z[i] for i in range(len(z)) if i not in samples]
- 
+    filter = np.array([True for e in x])
+    filter[samples]=False
+    cx = x[filter]
+    cy = y[filter]
+    cz = z[filter]
+    #plt.scatter(cx,cy,cz, c='r',marker='o') 
     ax.scatter(cx,cy,cz, c='r',marker='o')
-    sx = [x[i] for i in samples] 
-    sy = [y[i] for i in samples] 
-    sz = [z[i] for i in samples] 
+    sx = x[samples]
+    sy = y[samples]
+    sz = z[samples]
+        
+    #fig.add_subplot(122,projection='3d')
     ax.scatter(sx,sy,sz, c='g',marker='o')
     ax.set_xlabel('X label')
     ax.set_ylabel('y label')
@@ -69,16 +100,10 @@ def farthest_point_sampling(vertices, triangles, num_sample=15):
     mask = [True for e in vertex_indices]
     samples = np.array([], dtype=np.int32)
     idx = np.random.randint(0, len(vertex_indices))
-    # for debug 
-    #idx = 0
     idx = np.int32(idx)
     samples = np.append(samples, idx)
     for e in samples:
         mask[e] = False
-    # TODO compute farthest point and append to samples
-    #remaining_index = np.array([e for e in vertex_indices if e not in samples], dtype=np.int32)
-    #print(remaining_index)
-    #dist_array = geo_dist(vertices, triangles, source_indices=samples)
     # calculate farthes point to samples
     dist_array = geo_dist(vertices, triangles, source_indices=samples, target_indices=vertex_indices)
     for i in range(1, len(dist_array)):
@@ -87,33 +112,52 @@ def farthest_point_sampling(vertices, triangles, num_sample=15):
     max_idx = np.argmax(dist_array)
     samples = np.append(samples, np.int32(max_idx))
     mask[max_idx] = False
-    #print(len(vertices))
-    # TODO compute remianing points set distance for each sample,select the nearest 
     for iter in range(num_sample-2):
-
         dist_array = np.array([], dtype=np.float64)
         for i in range(len(vertex_indices)):
-            #print('samples = {0}'.format(samples))
             dist = geo_dist(vertices, triangles, source_indices=vertex_indices[i:i+1], target_indices=samples)
-            #print('dist = {0}'.format(dist))
             dist_array = np.append(dist_array, dist)
         dist_array = np.reshape(dist_array, (-1, len(samples)))
-        #print('dist_array = {0}'.format(dist_array))
         max_min = max_min_args(dist_array, samples) 
         samples = np.append(samples, np.int32(max_min))
         mask[max_min] = False
-    print('samples = {0}'.format(samples))
-    print('len samples = {0}'.format(len(samples)))
-    fps_vis(vertices, samples)
-        #print(max_min)
+    return samples
+# TODO given array of vertices and adjacency matrix, return neighbouring vertices
+#def vertex_neighbors(vertices, adjacent_matrix, depths = 3):
+#    return vertices
 
-
-    # TODO select farthes in remaining points
+# TODO given a triangle mesh, return sparse adjacency list
+# TODO eliminate waring 
+def sparse_adjacency_matrix(mesh):
+    vertices = np.asarray(mesh.vertices, dtype=np.float)
+    triangles = np.asarray(mesh.triangles, dtype=np.int)
+    matrix = []
+    for i in range(len(vertices)):
+        matrix.append(np.array([], dtype=np.int32))
+    for e in triangles:
+        a, b, c = e[0], e[1], e[2]
+        matrix[a] = np.append(matrix[a], [b,c])
+        matrix[b] = np.append(matrix[b], [a,c])
+        matrix[c] = np.append(matrix[c], [a,b])
+    for e in range(len(matrix)):
+        matrix[e] = np.unique(matrix[e])
+    return matrix
 
 if __name__=='__main__':
 
-    mesh = meshio.read('./data/half_polyhedron.obj')
-    vertices = np.array(mesh.points, dtype=np.float64)
-    triangles = np.array(mesh.cells[0][1], dtype=np.int32)
-    farthest_point_sampling(vertices, triangles, 15)
-    #fps_vis(vertices, samples)
+   mesh = o3d.io.read_triangle_mesh('./data/half_polyhedron.obj') 
+   mesh.compute_triangle_normals()
+   mesh.compute_vertex_normals()
+
+   vertices = np.asarray(mesh.vertices,dtype=np.float64)
+   triangles = np.asarray(mesh.triangles,dtype=np.int32)
+   # if not mesh.has_vertex_normals():
+   vertex_normals = mesh.vertex_normals
+   # if not mesh.has_triangle_normals():
+   triangle_normals = mesh.triangle_normals
+   #samples = farthest_point_sampling(vertices, triangles)
+   #fps_vis(vertices, samples)
+   #o3d.visualization.draw_geometries([mesh])
+   sparse_matrix = sparse_adjacency_matrix(mesh)
+   print(np.asarray(sparse_matrix))
+   
